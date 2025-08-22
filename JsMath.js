51db7e476ea6df51a8cab9a846b7e5f970673e07
ZephyrAmmor <amorzephyr@gmail.com>
2025-08-22 21:16:56 +0500

    Define Constants

---
+++ JsMath.js
@@ -0,0 +1,11 @@
+const JsMath = {
+    E : 2.71828,
+    LN2 : 2.30259,
+    LN10 : 2.30259,
+    LOG10E : 1.44270,
+    PI : 3.14159,
+    SQRT1_2 : 0.70711,
+    SQRT2 : 1.41421,
+
+}
+console.table(JsMath)
