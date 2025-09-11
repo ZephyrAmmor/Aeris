a46cf010e90db1e80c94e25a56efdb3522963ab1
ZephyrAmmor <amorzephyr@gmail.com>
2025-09-11 10:41:38 +0500

    Add a basic search and a new gif button

--- apis/giphy.js
+++ apis/giphy.js
@@ -1,8 +1,36 @@
- const image = document.querySelector('img')
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=Ak5tiNQ1k7pQgPKCcTmmn4kvrxJ0t9oM&s=cat')
-        .then(function(response) {
-           return (response.json())
-        })
-        .then(function(response) {
-            image.src = response.data.images.original.url
-        })
+const image = document.querySelector('img')
+const search = document.querySelector('.search')
+const searchBtn = document.querySelector('#search-btn')
+const newGif = document.querySelector('#new-gif')
+const error = document.querySelector('.error')
+let value = '404';
+fetch('https://api.giphy.com/v1/gifs/translate?api_key=Ak5tiNQ1k7pQgPKCcTmmn4kvrxJ0t9oM&s=cat')
+    .then(function(response) {
+        return (response.json())
+    })
+    .then(function(response) {
+        image.src = response.data.images.original.url
+    })
+searchBtn.addEventListener('click', () =>{
+    value = search.value || '404'
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Ak5tiNQ1k7pQgPKCcTmmn4kvrxJ0t9oM&s=${value}`)
+      .then(function(response){
+        return response.json()
+      })
+      .then(function(response){
+        image.src = response.data.images.original.url
+      })
+      .catch(function(err) {
+        error.textContent(`An Error occuered while fetching this data : ${err}`)
+      })
+})
+
+newGif.addEventListener('click',() =>{
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Ak5tiNQ1k7pQgPKCcTmmn4kvrxJ0t9oM&s=${value}`)
+    .then (function(response) {
+        return response.json()
+    })
+    .then (function(response) {
+        image.src = response.data.images.original.url
+    })
+} )
